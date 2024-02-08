d1364ef6a93e41ab7b035468332ae93cf0529fd1
Arjit Kedia <arjitkedia982@gmail.com>
2024-02-09 00:41:21 +0530

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -11,7 +11,7 @@ const bodyParser = require("body-parser");
 connect()
 app.use(
   cors({
-    origin: ["http://localhost:5173", "http://localhost:5174","https://adminportfolio.vercel.app"],
+    origin: ["http://localhost:5173", "http://localhost:5174","https://adminportfolio.vercel.app","https://arjit-portfolio.vercel.app"],
     methods: ["GET", "POST", "PATCH", "DELETE"],
   })
 );
