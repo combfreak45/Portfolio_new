08553027ffbead8e1246633ad92efcce4a603ff6
Arjit Kedia <arjitkedia982@gmail.com>
2024-02-09 00:38:49 +0530

    Update Skills.jsx

--- main/src/Components/Skills.jsx
+++ main/src/Components/Skills.jsx
@@ -8,7 +8,7 @@ const Skills = () => {
   useEffect(() => {
     const getData = async () => {
       try {
-        const response = await axios.get("http://localhost:5000/skill");
+        const response = await axios.get("https://portfolio-new-ashen-kappa.vercel.app/skill");
         if (response.data.message === "ok") {
           setData(response.data.skills);
         } else {
